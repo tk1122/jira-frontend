388ffdd68b2fb8b37b609390559c4aeb97aca824
nhatvu1998 <nhatvu28998@gmail.com>
2020-05-19 14:09:21 +0700

    JIRA-52: add http error interceptor

---
+++ src/shared/http-interceptor/error-interceptors.ts
@@ -0,0 +1,37 @@
+import {
+  HttpEvent,
+  HttpInterceptor,
+  HttpHandler,
+  HttpRequest,
+  HttpResponse,
+  HttpErrorResponse
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+import {NzNotificationService} from "ng-zorro-antd/notification";
+
+export class HttpErrorInterceptor implements HttpInterceptor {
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request)
+      .pipe(
+        retry(1),
+        catchError((error: HttpErrorResponse) => {
+          let errorMessage = '';
+          console.log(error)
+          if (error.error instanceof ErrorEvent) {
+            // client-side error
+            errorMessage = `Error: ${error.error.message}`;
+          } else {
+            // server-side error
+            errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+          }
+          // this.notification.create(
+          //   'error',
+          //   'Đăng nhập thất bại',
+          //   ''
+          // );
+          return throwError(errorMessage);
+        })
+      )
+  }
+}
