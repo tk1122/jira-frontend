caa774afee1b2aa61f0f848c4699fc158f851e2b
Ngô Trường Sinh <sinhnt@bap.jp>
2020-05-27 14:37:42 +0700

    JIRA-64: dispatch multiple actions on loadEpics effect

--- src/app/module/epic/epic.effects.ts
+++ src/app/module/epic/epic.effects.ts
@@ -1,15 +1,13 @@
 import {Injectable} from '@angular/core';
 import {Actions, createEffect, ofType} from '@ngrx/effects';
 import {loadEpicFailure, loadEpics, loadEpicsSuccess} from "./epic.actions";
-import {catchError, filter, map, switchMap, tap, withLatestFrom} from "rxjs/operators";
+import {catchError, filter, map, mergeMap, switchMap, tap, withLatestFrom} from "rxjs/operators";
 import {EpicService} from "./epic.service";
 import {UserService} from "../user/user.service";
 import {select, Store} from "@ngrx/store";
-import {epics, isEpicsLoaded} from "./epic.selectors";
 import {ErrorMessage} from "../../../shared/model/error-message";
 import {of} from "rxjs";
 import {loadIssuesByProjectId, loadIssuesByProjectIdSuccess} from "../issue/issue.actions";
-import {isProjectIssuesLoaded} from "../issue/issue.selectors";
 import {IssueService} from "../issue/issue.service";
 import {projectSelectedId} from "../project/project.selectors";
 import {selectProject} from "../project/project.actions";
@@ -28,18 +26,13 @@ export class EpicEffects {
         return !projectSelectedId || Number(projectId) !== projectSelectedId
       }),
       switchMap(([action, _]) =>
-        [this.epicService.getEpicByProjectId(action.projectId.toString()).pipe(
-          map(epics => {
-            return loadEpicsSuccess({epics})
-          }),
-          tap(epics => {
-            console.log(epics)
+        this.epicService.getEpicByProjectId(action.projectId.toString()).pipe(
+          mergeMap(epics => {
+            return [loadEpicsSuccess({epics}), selectProject({id: Number(action.projectId)})]
           }),
           catchError((err: ErrorMessage) => {
             return of(loadEpicFailure({message: err}))
-          }),
-            selectProject({id: Number(action.projectId)})
-          ]
+          })
         )
       )
     )
@@ -67,5 +60,4 @@ export class EpicEffects {
 
   constructor(private actions$: Actions, private readonly epicService: EpicService, private readonly userService: UserService, private readonly issueService: IssueService, private readonly store: Store) {
   }
-
 }
