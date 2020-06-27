410b5aae1c5efa8232b70502a62e2d39d5af08e2
Ngô Trường Sinh <sinhnt@bap.jp>
2020-06-27 12:18:27 +0700

    JIRA-77: add deploy configuration frontend

--- src/app/module/issue/component/issue-list/issue-list.component.ts
+++ src/app/module/issue/component/issue-list/issue-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Issue, IssueStatus} from "../../../../../shared/model/issue";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-issue-list',
@@ -10,10 +11,13 @@ export class IssueListComponent implements OnInit {
   @Input('data')
   issues: Issue[] = [];
 
-  constructor() { }
+  constructor(private readonly router: Router) { }
 
   panigation: boolean = this.issues.length > 10
   ngOnInit(): void {
   }
 
+  editIssue(id: number) {
+    this.router.navigate(['/issues', id]).then()
+  }
 }
